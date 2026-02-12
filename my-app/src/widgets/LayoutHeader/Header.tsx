ab4398efe8fa1269987e04a145f176f7f56e1e33
Ilia Tretiak <nba-91@mail.ru>
2026-02-12 14:12:13 +0400

    Added Compound Components with Modal, HOC with PostList, CommentList, filterByLength, optimisation with useMemo

--- my-app/src/widgets/LayoutHeader/Header.tsx
+++ my-app/src/widgets/LayoutHeader/Header.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import Button from '../../shared/ui/Button/Button'
-import ModalInfo from '../../shared/ui/Modal/ModalInfo'
+import Modal from '../../shared/ui/Modal/Modal'
 import ThemeSwitcher from '../../features/ThemeSwitcher/ui/ThemeSwitcher'
 import style from './header.module.css'
 import style__button from '../../shared/ui/Button/button.module.css'
-
 
 function Header() {
 	const [isOpen, setIsOpen] = useState<boolean>(false)
 	const styles: string = style__button.button
 	return (
 		<div className={style.header}>
-			<Button styles={styles} onclick={() => setIsOpen(!isOpen)} children={"О проекте"} />
-			{isOpen ? <ModalInfo onclick={() => setIsOpen(!isOpen)} /> : ''}
+			<Button styles={styles} onclick={() => setIsOpen(true)} children={"О проекте"} />
+			<Modal isOpen={isOpen} onclick={() => setIsOpen(!isOpen)}>
+				<Modal.Header>
+					<h2>О проекте</h2>
+				</Modal.Header>
+				<Modal.Body>
+					<p>Это учебный проект в рамках интенсива по React компании Astons</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<p>выполняет Третьяк Илья</p>
+				</Modal.Footer>
+			</Modal>
 			<ThemeSwitcher />
 		</div>
 	)
